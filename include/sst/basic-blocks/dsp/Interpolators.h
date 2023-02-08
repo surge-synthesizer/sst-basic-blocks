7b27c93c8fac31263652d2a04c88b4a286482207
Paul <baconpaul@users.noreply.github.com>
2023-02-08 18:12:54 -0500

    Port the Rack simple modulators (#1)
    Port enough of the DSP and Rack Modulation environment so
    QuadLFO, QuadAD and EGxVCA can run off of this module.

---
+++ include/sst/basic-blocks/dsp/Interpolators.h
@@ -0,0 +1,37 @@
+/*
+ * sst-basic-blocks - a Surge Synth Team product
+ *
+ * Provides basic tools and blocks for use on the audio thread in
+ * synthesis, including basic DSP and modulation functions
+ *
+ * Copyright 2019 - 2023, Various authors, as described in the github
+ * transaction log.
+ *
+ * sst-basic-blocks is released under the Gnu General Public Licence
+ * V3 or later (GPL-3.0-or-later). The license is found in the file
+ * "LICENSE" in the root of this repository or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * All source for sst-basic-blocks is available at
+ * https://github.com/surge-synthesizer/sst-basic-blocks
+ */
+#ifndef SST_BASIC_BLOCKS_DSP_INTERPOLATORS_H
+#define SST_BASIC_BLOCKS_DSP_INTERPOLATORS_H
+
+namespace sst::basic_blocks::dsp
+{
+inline float cubic_ipol(float y0, float y1, float y2, float y3, float mu)
+{
+    float a0, a1, a2, a3, mu2;
+
+    mu2 = mu * mu;
+    a0 = y3 - y2 - y0 + y1;
+    a1 = y0 - y1 - a0;
+    a2 = y2 - y0;
+    a3 = y1;
+
+    return (a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3);
+}
+}
+
+#endif // SURGEXTRACK_INTERPOLATORS_H
---
+++ tests/smoketest.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Paul Walker on 2/8/23.
+//
+
+// For now just a simple smoke test
+
+
+#if defined(__arm64__)
+#define SIMDE_ENABLE_NATIVE_ALIASES
+#include "simde/x86/sse2.h"
+#else
+#include <emmintrin.h>
+#endif
+
+#include "sst/basic-blocks/dsp/CorrelatedNoise.h"
+#include "sst/basic-blocks/dsp/Interpolators.h"
+
+#include "sst/basic-blocks/modulators/ADAREnvelope.h"
+#include "sst/basic-blocks/modulators/ADSRDAHDEnvelope.h"
+#include "sst/basic-blocks/modulators/SimpleLFO.h"
+
+#include <memory>
+
+int main(int argc, char **argv)
+{
+    using namespace sst::basic_blocks;
+
+    struct SampleSRProvider
+    {
+        double samplerate{44100};
+        float envelope_rate_linear_nowrap(float f) const { return 0; }
+    };
+
+    auto srp = std::make_unique<SampleSRProvider>();
+    auto adsr = sst::basic_blocks::modulators::ADSRDAHDEnvelope<SampleSRProvider, 16>(srp.get());
+    adsr.process(0,0,0,0,0,0,0,0);
+    auto ad = sst::basic_blocks::modulators::ADAREnvelope<SampleSRProvider, 16>(srp.get());
+    ad.process(0,0,0,0,false);
+    auto lf = sst::basic_blocks::modulators::SimpleLFO<SampleSRProvider, 16>(srp.get());
+    lf.process_block(0,0,0);
+
+}
