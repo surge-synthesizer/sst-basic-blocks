31ef1838219accf1a69268839aaab4d59d44400a
Paul Walker <paul@pwjw.com>
2023-03-21 10:12:02 -0400

    q

--- include/sst/basic-blocks/modulators/AHDSRShapedSC.h
+++ include/sst/basic-blocks/modulators/AHDSRShapedSC.h
@@ -25,7 +25,7 @@
 namespace sst::basic_blocks::modulators
 {
 template <typename SRProvider, int BLOCK_SIZE, typename RangeProvider = TenSecondRange>
-struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
+struct AHDSRShapedSC : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
 {
     using base_t = DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>;
 
@@ -35,7 +35,7 @@ struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
     static inline bool lutsInitialized{false};
 
     SRProvider *srProvider{nullptr};
-    AHDSRShapedLUT(SRProvider *s)
+    AHDSRShapedSC(SRProvider *s)
         : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>(), srProvider(s)
     {
         assert(srProvider);
@@ -46,7 +46,6 @@ struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
         if (lutsInitialized)
             return;
 
-        std::cout << "INITIALIZING LUTS" << std::endl;
         lutsInitialized = true;
     }
 
@@ -62,6 +61,125 @@ struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
         return srProvider->envelope_rate_linear_nowrap(x * base_t::etScale + base_t::etMin);
     }
 
+    // from https://martin.ankerl.com/2012/01/25/optimized-approximative-pow-in-c-and-cpp/
+    // also check out https://www.hxa.name/articles/content/fast-pow-adjustable_hxa7241_2007.html
+    inline double fastPow(double a, double b)
+    {
+        static_assert(sizeof(double) == 2 * sizeof(int32_t));
+        union
+        {
+            double d;
+            int32_t x[2];
+        } u = {a};
+        u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
+        u.x[0] = 0;
+        return u.d;
+    }
+
+    // Shape is -1,1; phase is 0,1
+    inline float kernel(float phase, float shape)
+    {
+        auto b = -shape * 8.f;
+        auto c = (b < 0 ? (1.f / (1 - b) - 1) : b);
+        auto r = std::pow(phase, (1.f + c));
+        return r;
+    }
+
+    inline void processCore(const float a, const float h, const float d, const float s,
+                            const float r, const float ashape, const float dshape,
+                            const float rshape, const bool gateActive)
+    {
+        float target = 0;
+
+        auto &stage = this->stage;
+
+        if (!gateActive && stage < base_t::s_release)
+        {
+            stage = base_t::s_release;
+            releaseStartValue = this->outputCache[0];
+            phase = 0;
+        }
+
+        /*
+         * We can simply inline this here since there's no switches really
+         */
+        switch (stage)
+        {
+        case base_t::s_attack:
+        {
+            phase += dPhase(a);
+            if (phase > 1)
+            {
+                if (h > base_t::etMin)
+                {
+                    stage = base_t::s_hold;
+                }
+                else
+                {
+                    stage = base_t::s_decay;
+                }
+                phase -= 1.f;
+                target = 1.0;
+            }
+            else
+            {
+                target = kernel(phase, ashape) * (1 - attackStartValue) + attackStartValue;
+            }
+        }
+        break;
+        case base_t::s_hold:
+        {
+            phase += dPhase(h);
+
+            target = 1;
+            if (phase > 1)
+            {
+                stage = base_t::s_decay;
+                phase -= 1.f;
+            }
+        }
+        break;
+        case base_t::s_decay:
+        {
+            phase += dPhase(d);
+            if (phase > 1)
+            {
+                stage = base_t::s_sustain;
+                target = s;
+            }
+            else
+            {
+                target = (1.0 - kernel(phase, dshape)) * (1.0 - s) + s;
+            }
+        }
+        break;
+        case base_t::s_sustain:
+        {
+            target = s;
+        }
+        break;
+        case base_t::s_release:
+        {
+            phase += dPhase(r);
+            if (phase > 1)
+            {
+                stage = base_t::s_complete;
+                target = 0;
+            }
+            else
+            {
+                target = (1 - kernel(phase, rshape)) * releaseStartValue;
+            }
+        }
+        break;
+        default:
+            target = 0.f;
+            break;
+        }
+
+        base_t::updateBlockTo(target);
+    }
+
     inline void process(const float a, const float h, const float d, const float s, const float r,
                         const float ashape, const float dshape, const float rshape,
                         const bool gateActive)
@@ -72,95 +190,7 @@ struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
 
         if (this->current == BLOCK_SIZE)
         {
-            float target = 0;
-
-            auto &stage = this->stage;
-
-            if (!gateActive && stage < base_t::s_release)
-            {
-                stage = base_t::s_release;
-                releaseStartValue = this->output;
-                phase = 0;
-            }
-
-            /*
-             * We can simply inline this here since there's no switches really
-             */
-            switch (stage)
-            {
-            case base_t::s_attack:
-            {
-                phase += dPhase(a);
-                if (phase > 1)
-                {
-                    if (h > base_t::etMin)
-                    {
-                        stage = base_t::s_hold;
-                    }
-                    else
-                    {
-                        stage = base_t::s_decay;
-                    }
-                    phase -= 1.f;
-                    target = 1.0;
-                }
-                else
-                {
-                    target = phase * (1 - attackStartValue) + attackStartValue;
-                }
-            }
-            break;
-            case base_t::s_hold:
-            {
-                phase += dPhase(h);
-
-                target = 1;
-                if (phase > 1)
-                {
-                    stage = base_t::s_decay;
-                    phase -= 1.f;
-                }
-            }
-            break;
-            case base_t::s_decay:
-            {
-                phase += dPhase(d);
-                if (phase > 1)
-                {
-                    stage = base_t::s_sustain;
-                    target = s;
-                }
-                else
-                {
-                    target = (1.0 - phase) * (1.0 - s) + s;
-                }
-            }
-            break;
-            case base_t::s_sustain:
-            {
-                target = s;
-            }
-            break;
-            case base_t::s_release:
-            {
-                phase += dPhase(r);
-                if (phase > 1)
-                {
-                    stage = base_t::s_complete;
-                    target = 0;
-                }
-                else
-                {
-                    target = (1 - phase) * releaseStartValue;
-                }
-            }
-            break;
-            default:
-                target = 0.f;
-                break;
-            }
-
-            base_t::updateBlockTo(target);
+            processCore(a, h, d, s, r, ashape, rshape, dshape, gateActive);
         }
 
         base_t::step();
@@ -170,12 +200,7 @@ struct AHDSRShapedLUT : DiscreteStagesEnvelope<BLOCK_SIZE, RangeProvider>
                              const float r, const float ashape, const float dshape,
                              const float rshape, const bool gateActive)
     {
-        this->current = BLOCK_SIZE;
-        process(a, h, d, s, r, ashape, dshape, rshape, gateActive);
-        if (this->stage == base_t::s_complete || this->stage == base_t::s_eoc)
-        {
-            memset(this->outputCache, 0, sizeof(this->outputCache));
-        }
+        processCore(a, h, d, s, r, ashape, dshape, rshape, gateActive);
     }
 
     float phase{0.f}, attackStartValue{0.f}, releaseStartValue{0.f};
