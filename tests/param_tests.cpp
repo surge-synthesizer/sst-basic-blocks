e2d8d51014b18aa0acebea04706d01cd77fd5eec
Paul Walker <paul@pwjw.com>
2023-04-27 07:57:50 -0400

    ParameterMetaData gets String Conversion functions
    Add a set of parameter metadata string bidirectional
    conversion functions; start basic implementations.

---
+++ tests/param_tests.cpp
@@ -0,0 +1,69 @@
+/*
+ * sst-basic-blocks - an open source library of core audio utilities
+ * built by Surge Synth Team.
+ *
+ * Provides a collection of tools useful on the audio thread for blocks,
+ * modulation, etc... or useful for adapting code to multiple environments.
+ *
+ * Copyright 2023, various authors, as described in the GitHub
+ * transaction log. Parts of this code are derived from similar
+ * functions original in Surge or ShortCircuit.
+ *
+ * sst-basic-blocks is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * All source in sst-basic-blocks available at
+ * https://github.com/surge-synthesizer/sst-basic-blocks
+ */
+
+#include "catch2.hpp"
+#include "smoke_test_sse.h"
+#include <cmath>
+#include <iostream>
+
+#include "sst/basic-blocks/params/ParamMetadata.h"
+
+namespace pmd = sst::basic_blocks::params;
+TEST_CASE("Percent and BiPolar Percent")
+{
+    SECTION("Percent")
+    {
+        auto p = pmd::ParamMetaData().asPercent();
+        std::string ems;
+        REQUIRE(p.minVal == 0.f);
+        REQUIRE(p.maxVal == 1.f);
+        REQUIRE(p.naturalToNormalized01(0.37f) == 0.37f);
+        REQUIRE(p.supportsStringConversion);
+        REQUIRE(p.valueToString(0.731) == "73.10 %");
+        REQUIRE(p.valueToString(0.03123456, true) == "3.123456 %");
+        REQUIRE(*(p.valueFromString(*(p.valueToString(0.731)), ems)) == 0.731f);
+        REQUIRE(p.modulationNaturalToString(0, 0.2, true)->summary == "+/- 20.00 %");
+    }
+    SECTION("PercentBipolar")
+    {
+        auto p = pmd::ParamMetaData().asPercentBipolar();
+        std::string ems;
+
+        REQUIRE(p.minVal == -1.f);
+        REQUIRE(p.maxVal == 1.f);
+        REQUIRE(p.naturalToNormalized01(0.37f) == 0.5 + 0.5 * 0.37f);
+        REQUIRE(p.supportsStringConversion);
+        REQUIRE(p.valueToString(0.731) == "73.10 %");
+        REQUIRE(*(p.valueFromString(*(p.valueToString(0.731)), ems)) == 0.731f);
+        REQUIRE(p.modulationNaturalToString(0, 0.2, true)->summary == "+/- 20.00 %");
+    }
+
+    SECTION("Audible Frequency")
+    {
+        auto p = pmd::ParamMetaData().asAudibleFrequency();
+
+        std::string ems;
+        REQUIRE(p.supportsStringConversion);
+        REQUIRE(*(p.valueToString(0)) == "440.00 Hz");
+        REQUIRE(*(p.valueToString(12)) == "880.00 Hz");
+        REQUIRE(*(p.valueFromString("440", ems)) == 0);
+        REQUIRE(*(p.valueFromString("220", ems)) == -12);
+    }
+}
