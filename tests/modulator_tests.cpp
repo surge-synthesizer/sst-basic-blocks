667f32b999c9f85d7fafb125df1f6a196b54c840
Paul Walker <paul@pwjw.com>
2023-09-30 08:56:17 -0400

    Bring the FX Low Frequency Modulator here
    - the one used in phaser, chorus, etc...

---
+++ tests/modulator_tests.cpp
@@ -0,0 +1,50 @@
+/*
+ * sst-basic-blocks - an open source library of core audio utilities
+ * built by Surge Synth Team.
+ *
+ * Provides a collection of tools useful on the audio thread for blocks,
+ * modulation, etc... or useful for adapting code to multiple environments.
+ *
+ * Copyright 2023, various authors, as described in the GitHub
+ * transaction log. Parts of this code are derived from similar
+ * functions original in Surge or ShortCircuit.
+ *
+ * sst-basic-blocks is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * All source in sst-basic-blocks available at
+ * https://github.com/surge-synthesizer/sst-basic-blocks
+ */
+
+#include "catch2.hpp"
+#include "smoke_test_sse.h"
+#include "sst/basic-blocks/modulators/FXModControl.h"
+
+namespace smod = sst::basic_blocks::modulators;
+
+TEST_CASE("Mod LFO Is Well Behaved", "[mod]")
+{
+    for (int m = smod::FXModControl::mod_sine; m <= smod::FXModControl::mod_square; ++m)
+    {
+        DYNAMIC_SECTION("Mod Control Bounded For Type " << m)
+        {
+            for (int tries = 0; tries < 500; ++tries)
+            {
+                float rate = (float)rand() / (float)RAND_MAX * 10;
+                float phase_offset = 0.0999 * (tries % 10);
+                float depth = 1.0;
+
+                INFO("200 Samples at " << rate << " with po " << phase_offset);
+                smod::FXModControl mc(48000, 1.0 / 48000);
+                for (int s = 0; s < 200; ++s)
+                {
+                    mc.pre_process(m, rate, depth, phase_offset);
+                    REQUIRE(mc.value() <= 1.0);
+                    REQUIRE(mc.value() >= -1.0);
+                }
+            }
+        }
+    }
+}
