3987201242f94a59bf33c0073a26728898603201
Paul Walker <paul@pwjw.com>
2023-11-24 08:49:19 -0500

    License; Exceptions on MacOS
    - Clarify that the package is GPL3 but that also a few files are
      available to be copied in an MIT fashion
    - Change the CATCH exception override to not be used if OSX deployment
      greater than 10.12

--- include/sst/basic-blocks/dsp/PanLaws.h
+++ include/sst/basic-blocks/dsp/PanLaws.h
@@ -12,7 +12,13 @@
  * sst-basic-blocks is released under the GNU General Public Licence v3
  * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
  * file in the root of this repository, or at
- * https://www.gnu.org/licenses/gpl-3.0.en.html
+ * https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * A very small number of explicitly chosen header files can also be
+ * used in an MIT/BSD context. Please see the README.md file in this
+ * repo or the comments in the individual files. Only headers with an
+ * explicit mention that they are dual licensed may be copied and reused
+ * outside the GPL3 terms.
  *
  * All source in sst-basic-blocks available at
  * https://github.com/surge-synthesizer/sst-basic-blocks
