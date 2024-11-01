12d063b807ee899d256faeea6ccf6da0d919be17
Paul Walker <paul@pwjw.com>
2024-11-01 08:37:10 -0400

    thisisit

--- include/sst/basic-blocks/simd/setup.h
+++ include/sst/basic-blocks/simd/setup.h
@@ -76,9 +76,7 @@
 
 #ifndef SST_SIMD_NATIVE_X86
 #ifndef SST_SIMD_OMIT_NATIVE_ALIASES
-// #define SIMDE_ENABLE_NATIVE_ALIASES
-#else
-#errir FOOBAZ
+#define SIMDE_ENABLE_NATIVE_ALIASES
 #endif
 #endif
 
